c9ea8cb207a7099083e745b4484f3d07f5bb8da4
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-03-30 11:15:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+
+
+echo "Cena vodky: 119 " . $vodkaPrice;
+
+echo "<br>Peníze bezdomovce před návštěvou večerky: 132 " . $homelessMoney . "<br>";
+
+if($homelessMoney > 132 ) {
+    echo "Vodka zakoupena!";
+ } elseif($homelessMoney < 119) {
+     echo "Nedostatek peněz";
+ } elseif ($vodkaPrice < 100) {
+    echo "Cena vodky spadla nyní si ji můžeš zakoupit";
+}
+
+?>
+
+
+
+</body>
+</html>
